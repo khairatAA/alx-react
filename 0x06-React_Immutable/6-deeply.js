c2bb86c4edd4f1ee4f69e5126db25f67adaafb00
Khairat Adesina <khairatadesina01@gmail.com>
2024-04-25 03:49:01 +0100

    add: Nested merge

---
+++ 0x06-React_Immutable/6-deeply.js
@@ -0,0 +1,17 @@
+import { Map, List } from 'immutable';
+
+export default function mergeDeeplyElements(page1, page2) {
+    const map1 = Map(page1)
+    const map2 = Map(page2)
+
+    const mergedMap = map1.mergeWith((oldVal, newVal) => {
+        if (Map.isMap(oldVal) && Map.isMap(newVal)) {
+            return oldVal.mergeWith((deepOldVal, deepNewVal) => deepOldVal.merge(deepNewVal), newVal);
+        }
+        return newVal;
+    }, map2);
+
+    const mergedList = mergedMap.toList();
+
+    return mergedList;
+}
